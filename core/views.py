a6fe4c6f9f290a14aaa2f79135e5da9d36cd7ca4
Rodrigo Delfino de Carvalho <rodrigoddc1@gmail.com>
2020-03-12 20:39:36 -0300

    FIXING some bugs engine db" ;

--- core/views.py
+++ core/views.py
@@ -1,5 +1,18 @@
 from django.views.generic import TemplateView, CreateView, ListView, DetailView
+from .models import Service, Employee
+
+
+class EmployeeList(ListView):
+    template_name = 'core/team'
+    model = Employee
 
 
 class IndexView(TemplateView):
     template_name = 'core/index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(IndexView, self).get_context_data(**kwargs)
+        context['services'] = Service.objects.all()
+        context['employees'] = Employee.objects.all()
+
+        return context
