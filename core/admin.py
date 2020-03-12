0d9ae05e62c11e18c733173d747781bdea1f7348
Rodrigo Delfino de Carvalho <rodrigoddc1@gmail.com>
2020-03-12 17:09:03 -0300

    QualquerCoisaNoTextoDoCommit

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Service, Role, Employee
 
-# Register your models here.
+
+@admin.register(Role)
+class RoleAdmin(admin.ModelAdmin):
+    list_display = ['role', 'active', 'created', 'modified']
+
+
+@admin.register(Service)
+class ServiceAdmin(admin.ModelAdmin):
+    list_display = ['service', 'icon', 'active', 'created', 'modified']
+
+
+@admin.register(Employee)
+class EmployeeAdmin(admin.ModelAdmin):
+    list_display = ['name', 'role', 'active', 'created', 'modified']
--- core/models.py
+++ core/models.py
@@ -1,3 +1,68 @@
+import uuid
 from django.db import models
+from stdimage import StdImageField
 
-# Create your models here.
+
+def get_file_path(_instance, filename):
+    ext = filename.split('.')[-1]
+    filename = f'{uuid.uuid4()}.{ext}'
+    return filename
+
+
+class Base(models.Model):
+    created = models.DateField('Created', auto_now_add=True)
+    modified = models.DateField('Modified', auto_now=True)
+    active = models.BooleanField('Active?', default=True)
+
+    class Meta:
+        abstract = True
+
+
+class Service(Base):
+    ICON_CHOICES = (
+        ('lni-cog', 'Engine'),
+        ('lni-stats-up', 'Chart'),
+        ('lni-users', 'User'),
+        ('lni-layers', 'Design'),
+        ('lni-mobile', 'Mobile'),
+        ('lni-rocket', 'Rocket'),
+    )
+    service = models.CharField('Service', max_length=100)
+    description = models.TextField('Description', max_length=200)
+    icon = models.CharField('Icon', max_length=12, choices=ICON_CHOICES)
+
+    class Meta:
+        verbose_name = 'Service'
+        verbose_name_plural = 'Services'
+
+    def __str__(self):
+        return self.service
+
+
+class Role(Base):
+    role = models.CharField('Role', max_length=100)
+
+    class Meta:
+        verbose_name = 'Role'
+        verbose_name_plural = 'Roles'
+
+    def __str__(self):
+        return self.role
+
+
+class Employee(Base):
+    name = models.CharField('Employee', max_length=100)
+    role = models.ForeignKey('core.Role', verbose_name='Role', on_delete=models.CASCADE)
+    bio = models.TextField('Bio', max_length=200)
+    image = StdImageField('Image', upload_to=get_file_path,
+                          variations={
+                              'thumbs':
+                                  {'width': 480, 'height': 480, 'crop': True}
+                          })
+    facebook = models.CharField('Facebook', max_length=100, default='#')
+    twitter = models.CharField('Twitter', max_length=100, default='#')
+    instagram = models.CharField('Instagram', max_length=100, default='#')
+
+    class Meta:
+        verbose_name = 'Employee'
+        verbose_name_plural = 'Employees'
