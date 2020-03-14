a7d9f4acb29e803255aa3aa616f9d338082e75e8
Rodrigo Delfino de Carvalho <rodrigoddc1@gmail.com>
2020-03-14 20:35:01 -0300

    heroku

---
+++ core/forms.py
@@ -0,0 +1,28 @@
+from django import forms
+from django.core.mail.message import EmailMessage
+
+
+class ContactForm(forms.Form):
+    """
+    Form class for sending emails from index page to contac/suport
+    of the plataform
+    """
+    name = forms.CharField(label='Name', max_length=100)
+    email = forms.EmailField(label='Email', max_length=100)
+    subject = forms.CharField(label='Subject', max_length=100)
+    message = forms.CharField(label='Message', widget=forms.Textarea())
+
+    def send_mail(self):
+        name = self.cleaned_data['name']
+        email = self.cleaned_data['email']
+        subject = self.cleaned_data['subject']
+        message = self.cleaned_data['message']
+
+        mail = EmailMessage(
+            subject=subject,
+            body=message,
+            from_email=email,
+            to=['contact@mydomain.com', ],
+            headers={'Replay-to': name}
+        )
+        mail.send()
